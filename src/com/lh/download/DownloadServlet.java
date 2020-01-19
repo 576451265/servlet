d7d239407c4bcd2a0d6cb0cd702c236fd28dbf02
576451265 <576451265@qq.com>
2020-01-19 17:37:13 +0800

    文件下载

---
+++ src/com/lh/download/DownloadServlet.java
@@ -0,0 +1,51 @@
+package com.lh.download;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @Description: 文件从下载
+ * @author LuoH
+ * @date 2020/01/19
+ */
+@WebServlet("/downloadServlet")
+public class DownloadServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.获取请求参数，文件名称
+        String filename = request.getParameter("filename");
+        //2.使用字节输入流加载文件进内存
+        //2.1找到文件服务器路径
+        ServletContext servletContext = this.getServletContext();
+        String realPath = servletContext.getRealPath("/img/" + filename);
+        System.out.println(realPath);
+        //2.2用字节流关联
+        FileInputStream fis = new FileInputStream(realPath);
+        //3.设置response的响应头
+        //3.1 设置响应头类型：content-type
+        String mimeType = servletContext.getMimeType(filename);//获取文件的mime类型
+        response.setHeader("content-type", mimeType);
+        //3.2 设置响应头打开方式：content-disposition
+        response.setHeader("content-disposition", "attachment;filename=" + filename);
+        //4.将输入流的数据写出到输出流中
+        ServletOutputStream sos = response.getOutputStream();
+        byte[] buff = new byte[1024 * 8];
+        int len = 0;//读取到的个数
+        while ((len = fis.read(buff)) != -1) {
+            sos.write(buff, 0, len);
+        }
+        fis.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
